72cd4a68e27e6b96e3892487d99a5c90ab1cfc03
Zhuo Chen <zhuochen0624@gmail.com>
2025-09-26 11:11:56 -0400

    feat: automatically update yaml for different USER

--- build_images.sh
+++ build_images.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-. ./config.sh
+set -e
 
-set -ex
-
-EXEC=docker
-
-USER="appnetorg"
-
+# --- Configuration ---
+USER="deskchen"
 TAG="latest"
+IMAGE="onlineboutique-grpc"
+YAML_DIR="kubernetes/apply"
+# ---
 
-# for i in productpage ratings reviews details
-for i in onlineboutique-grpc
-do
-  IMAGE=${i}
-  echo Processing image ${IMAGE}
-  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile .
-  $EXEC push "$USER"/"$IMAGE":"$TAG"
-  echo
-done
+# 1. Update Kubernetes YAML files to use the new image
+echo "Updating YAML files in $YAML_DIR..."
+NEW_IMAGE="$USER/$IMAGE:$TAG"
+# Find and replace the image line in all .yaml files in the directory
+sed -i.bak "s|image: .*/$IMAGE:.*|image: $NEW_IMAGE|g" $YAML_DIR/*.yaml
+# Remove the backup files created by sed
+rm -f $YAML_DIR/*.yaml.bak
+
+# 2. Build the Docker image
+echo "Building Docker image: $NEW_IMAGE"
+docker build -t "$NEW_IMAGE" -f Dockerfile .
+
+# 3. Push the Docker image
+echo "Pushing Docker image: $NEW_IMAGE"
+docker push "$NEW_IMAGE"
+
+echo "✅ Process complete."
